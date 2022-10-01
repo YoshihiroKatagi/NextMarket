05c0c36ce6800c5302b5de44cbd3f9ebfdc2607a
katagi <katagiyoshihiro@gmail.com>
2022-10-01 18:33:37 +0900

    completed backend and typescript

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,9 +1,11 @@
+import type { NextApiResponse } from "next"
 import jwt from "jsonwebtoken"
+import { ExtendedNextApiRequestAuth, DecodedType, ResMessageType } from "./types"
 
 const secret_key = "nextmarket"
 
-const auth = (handler) => {
-  return async(req, res) => {
+const auth = (handler: Function) => {
+  return async(req: ExtendedNextApiRequestAuth, res: NextApiResponse<ResMessageType>) => {
     if(req.method === "GET"){
       return handler(req, res)
     }
@@ -15,7 +17,7 @@ const auth = (handler) => {
     }
     try{
       const decoded =jwt.verify(token, secret_key)
-      req.body.email = decoded.email
+      req.body.email = (decoded as DecodedType).email
       return handler(req, res)
     }catch(err){
       return res.status(401).json({message: "トークンが正しくないので、ログインしてください"})
